87ff96d81cd9dd4d81ec4acdc2d5091e73e1ee4c
P1amyanka <54539323+P1amyanka@users.noreply.github.com>
2026-07-24 17:54:43 +0300

    bump prototype to v0.4

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  VERSION: 'v0.3',
+  VERSION: 'v0.4',
   ALLOCATION_VERSION: 'allocation-v0.2',
   SUPABASE_URL: 'https://rcnyixakafcpmigojfhe.supabase.co',
   SUPABASE_KEY: ['sb', 'publishable', 'FMXIkx8ruTWSbfK8MdkrTQ', 'atXVT5Wf'].join('_'),
