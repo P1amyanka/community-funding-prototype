1d8636fb3299f3792ba8566508ead8765f30a35c
P1amyanka <54539323+P1amyanka@users.noreply.github.com>
2026-08-19 22:47:16 +0300

    Remove obsolete rich text toolbar hook

--- js/app.js
+++ js/app.js
@@ -3,11 +3,10 @@ import { createRound } from './home.js?v=0.4.2';
 import { copyPaymentValue, refreshParticipant, submitProposal } from './participant.js?v=0.4.2';
 import { closeRound, copyInput, downloadCsv, downloadHistoryCsv, manager, showNextRoundForm, startNextRound } from './manager.js?v=0.4.2';
 import { route, router } from './router.js?v=0.4.2';
-import { formatRichText } from './utils.js?v=0.4.2';
 
 Object.assign(window, {
   state, createRound, submitProposal, refreshParticipant, copyPaymentValue, closeRound, copyInput,
-  downloadCsv, downloadHistoryCsv, manager, showNextRoundForm, startNextRound, route, formatRichText,
+  downloadCsv, downloadHistoryCsv, manager, showNextRoundForm, startNextRound, route,
 });
 window.addEventListener('hashchange', router);
 router();
