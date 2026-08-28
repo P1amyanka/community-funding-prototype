122975d060ffad36689f0e0e2145c996ecb42cdc
P1amyanka <54539323+P1amyanka@users.noreply.github.com>
2026-08-28 11:18:09 +0300

    Expose manager comment drawer actions

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
-import { state } from './state.js?v=0.4.2-r4';
-import { createRound } from './home.js?v=0.4.2-r4';
-import { copyPaymentValue, refreshParticipant, submitProposal } from './participant.js?v=0.4.2-r4';
-import { closeRound, copyInput, downloadCsv, downloadHistoryCsv, manager, showNextRoundForm, startNextRound } from './manager.js?v=0.4.2-r4';
-import { route, router } from './router.js?v=0.4.2-r4';
+import { state } from './state.js?v=0.4.2-r5';
+import { createRound } from './home.js?v=0.4.2-r5';
+import { copyPaymentValue, refreshParticipant, submitProposal } from './participant.js?v=0.4.2-r5';
+import { closeProposalComment, closeRound, copyInput, downloadCsv, downloadHistoryCsv, manager, showNextRoundForm, showProposalComment, startNextRound } from './manager.js?v=0.4.2-r5';
+import { route, router } from './router.js?v=0.4.2-r5';
 
 Object.assign(window, {
   state, createRound, submitProposal, refreshParticipant, copyPaymentValue, closeRound, copyInput,
-  downloadCsv, downloadHistoryCsv, manager, showNextRoundForm, startNextRound, route,
+  downloadCsv, downloadHistoryCsv, manager, showNextRoundForm, startNextRound, showProposalComment,
+  closeProposalComment, route,
 });
 window.addEventListener('hashchange', router);
 router();
