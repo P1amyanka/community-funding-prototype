e46faaacd9e99d97445baf9fe8faf8217d067a8a
P1amyanka <54539323+P1amyanka@users.noreply.github.com>
2026-09-17 15:26:14 +0300

    Expose manager auth actions

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
-import { state } from './state.js?v=0.4.2-r6';
-import { createRound } from './home.js?v=0.4.2-r6';
-import { copyPaymentValue, refreshParticipant, submitProposal } from './participant.js?v=0.4.2-r6';
-import { closeProposalComment, closeRound, copyInput, downloadCsv, downloadHistoryCsv, manager, showNextRoundForm, showProposalComment, startNextRound } from './manager.js?v=0.4.2-r6';
-import { route, router } from './router.js?v=0.4.2-r6';
+import { state } from './state.js?v=0.4.2-r7';
+import { createRound } from './home.js?v=0.4.2-r7';
+import { copyPaymentValue, refreshParticipant, submitProposal } from './participant.js?v=0.4.2-r7';
+import { closeProposalComment, closeRound, copyInput, downloadCsv, downloadHistoryCsv, manager, showNextRoundForm, showProposalComment, startNextRound } from './manager.js?v=0.4.2-r7';
+import { sendManagerMagicLink, signOutManager } from './auth.js?v=0.4.2-r7';
+import { route, router } from './router.js?v=0.4.2-r7';
 
 Object.assign(window, {
   state, createRound, submitProposal, refreshParticipant, copyPaymentValue, closeRound, copyInput,
   downloadCsv, downloadHistoryCsv, manager, showNextRoundForm, startNextRound, showProposalComment,
-  closeProposalComment, route,
+  closeProposalComment, sendManagerMagicLink, signOutManager, route,
 });
 window.addEventListener('hashchange', router);
 router();
